3ea6e1f484f4d4732a523c8ba692575ab826b01e
Camarederic <camarederic@mail.ru>
2022-11-11 22:27:41 +0300

    Create ViewModel

--- app/src/main/java/com/example/viewmodelcomponent_48/MainActivity.kt
+++ app/src/main/java/com/example/viewmodelcomponent_48/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.viewmodelcomponent_48
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+      lateinit var mainActivityViewModel: MainActivityViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mainActivityViewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+
+        textViewNumber.text = mainActivityViewModel.number.toString()
+
+        buttonAdd.setOnClickListener {
+            mainActivityViewModel.addNumber()
+            textViewNumber.text = mainActivityViewModel.number.toString()
+        }
+
     }
 }
